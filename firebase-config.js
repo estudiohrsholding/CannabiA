800dc20f1c7a156341431b844c35fa88d81dad86
estudiohrsholding <estudio.hrsholding@gmail.com>
2025-10-17 21:16:15 +0200

    sacando config de .gitignore

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCIRk9Vip1J5Zc8xAeObsU6poc-J8lcj0",
+    authDomain: "cannabia-9dfe3.firebaseapp.com",
+    projectId: "cannabia-9dfe3",
+    storageBucket: "cannabia-9dfe3.firebasestorage.app",
+    messagingSenderId: "605207982385",
+    appId: "1:605207982385:web:58254637495b2b6d2bc010",
+    measurementId: "G-6M1WRYG1PS"
+};
